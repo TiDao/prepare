14735090d90d9c4463e6c6caf506822ca807b537
tidao <tidao2049@gmail.com>
2021-07-19 18:39:26 +0800

    ...

--- localconf/chain_config_test.go
+++ localconf/chain_config_test.go
@@ -7,13 +7,13 @@ import (
 
 func TestReadAndWrite(t *testing.T) {
 	var config ChainConfig
-	err := config.ReadFile("../config/yml/bc1.yml")
+	err := config.ReadFile("../config/bc_4_7.yml")
 	if err != nil{
 		t.Error(err)
 	}
 	fmt.Println(config)
 
-	err = config.WriteFile("../config/yml/test-bc1.yaml",0664)
+	err = config.WriteFile("../config/test-bc1.yaml",0664)
 	if err != nil{
 		t.Error(err)
 	}
--- localconf/chainmaker_yaml_types_test.go
+++ localconf/chainmaker_yaml_types_test.go
@@ -2,29 +2,31 @@ package localconf
 
 import(
 	"testing"
-	"fmt"
-	"encoding/json"
-	"bytes"
+	//"fmt"
+	//"encoding/json"
+	//"bytes"
 )
 
-func formatStruct(t interface{}){
-	bs,_ := json.Marshal(t)
-	var out bytes.Buffer
-	json.Indent(&out,bs,"","\t")
-	fmt.Printf("%v\n",out.String())
-}
+//func formatStruct(t interface{}){
+//	bs,_ := json.Marshal(t)
+//	var out bytes.Buffer
+//	json.Indent(&out,bs,"","\t")
+//	fmt.Printf("%v\n",out.String())
+//}
 
 func TestTypes(t *testing.T) {
 	var setting = &CMConfig{}
-	if err := setting.ReadFile("../config/yml/chainmaker.yml"); err != nil {
+	if err := setting.ReadFile("../config/chainmaker.yml"); err != nil {
 		t.Error(err)
 	}
+
+	//formatStruct(setting)
 
 	if err := setting.ReadFile("../config/log.yml");err != nil{
 		t.Error(err)
 	}
 
-	if err := setting.WriteFile("../config/yml/test-1.yml",0664); err != nil{
+	if err := setting.WriteFile("../config/test-1.yml",0664); err != nil{
 		t.Error(err)
 	}
 
