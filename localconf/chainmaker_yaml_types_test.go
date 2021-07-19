039a8ac4eda656214454ca105b874a101caec6ed
tidao <tidao2049@gmail.com>
2021-07-19 21:39:21 +0800

    *

--- localconf/chainmaker_yaml_types_test.go
+++ localconf/chainmaker_yaml_types_test.go
@@ -3,33 +3,31 @@ package localconf
 import(
 	"testing"
 	"fmt"
-	//"encoding/json"
-	//"bytes"
+	"encoding/json"
+	"bytes"
 )
 
-//func formatStruct(t interface{}){
-//	bs,_ := json.Marshal(t)
-//	var out bytes.Buffer
-//	json.Indent(&out,bs,"","\t")
-//	fmt.Printf("%v\n",out.String())
-//}
+func formatStruct(t interface{}){
+	bs,_ := json.Marshal(t)
+	var out bytes.Buffer
+	json.Indent(&out,bs,"","\t")
+	fmt.Printf("%v\n",out.String())
+}
 
 func TestTypes(t *testing.T) {
 	var setting = &CMConfig{}
 	if err := setting.ReadFile("../config/chainmaker.yml"); err != nil {
-		fmt.Println("chainmaker.yml")
-		t.Error("unmarsh chainamker.yml",err)
+		t.Errorf("read chainmaker.yml error: %v",err)
 	}
 
-	//formatStruct(setting)
+	formatStruct(setting)
 
 	if err := setting.ReadFile("../config/log.yml");err != nil{
-		fmt.Println("log.yml")
-		t.Error("unmarshal log.yml: ",err)
+		t.Errorf("read log.yml error: %v",err)
 	}
 
 	if err := setting.WriteFile("../config/test-1.yml",0664); err != nil{
-		t.Error("write", err)
+		t.Errorf("write test-1.yml error: %v",err)
 	}
 
 }
